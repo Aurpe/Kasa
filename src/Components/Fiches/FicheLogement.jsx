c9ffe2e63d5ca68a6e68d3c18e8468e63c731343
Aurpe <160005299+Aurpe@users.noreply.github.com>
2024-09-13 13:57:58 +0200

    add file

---
+++ src/Components/Fiches/FicheLogement.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export default function FicheLogement() {
+  return (
+    <div>
+      <h1>Nos logements</h1>
+    </div>
+  )
+}
+
+
+/*import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+const FicheLogement = () => {
+  const { id } = useParams(); // Récupérer l'ID du logement depuis l'URL
+  const [logement, setLogement] = useState(null);
+
+  useEffect(() => {
+    fetch("/logements.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const selectedLogement = data.find((logement) => logement.id === parseInt(id));
+        setLogement(selectedLogement);
+      })
+      .catch((error) => console.error("Erreur lors du chargement du logement :", error));
+  }, [id]);
+
+  if (!logement) {
+    return <div>Chargement...</div>;
+  }
+
+  return (
+    <div>
+      <h1>{logement.title}</h1>
+      <img src={logement.imageUrl} alt={logement.title} />
+      <p>{logement.description}</p>
+    </div>
+  );
+};
+
+export default FicheLogement;*/
---
+++ src/Components/home/home/homePage.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Site Kasa</h1>
+        </div>
+    )
+};
+
