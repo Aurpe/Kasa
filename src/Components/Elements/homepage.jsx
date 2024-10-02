00873f13ee26520c3bddb2817965021cb74d8123
Aurpe <160005299+Aurpe@users.noreply.github.com>
2024-10-02 13:38:59 +0200

    MAJ scss mediaqueries

--- src/Components/Elements/homepage.jsx
+++ src/Components/Elements/homepage.jsx
@@ -9,18 +9,21 @@ import Banner from '../Elements/banner';
 
 function LogementCard({ id, cover, title }) {
   return (
-    <Link to={`/location/${id}`} className="logement-card">
-      <img src={cover} alt={title} className="logement-image" />
-      <div className="logement-title">{title}</div>
-    </Link>
+    <div className="Container-card"> 
+        <Link to={`/location/${id}`} className="logement-card">
+          <img src={cover} alt={title} className="logement-image" />
+          <div className="logement-title">{title}</div>
+        </Link>
+    </div>
+    
   );
 }
-
 export default function HomePage() {
   return (
     <div className="homepage">
       <Banner img="./src/assets/Images/Banner_homepage.png" title= "Chez vous, et partout ailleurs" />
       <main className="logements-grid">
+       
         {Data.map((logement) => (
           <LogementCard
             key={logement.id}
@@ -28,7 +31,9 @@ export default function HomePage() {
             cover={logement.cover}
             title={logement.title}
           />
+        
         ))}
+      
       </main>
     </div>
   );
