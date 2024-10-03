3f461fb03d996e4ae21111f9652d3d1a2d7b2d47
Aurpe <160005299+Aurpe@users.noreply.github.com>
2024-10-03 14:29:05 +0200

    mediaquery scss

--- src/Components/Elements/banner.jsx
+++ src/Components/Elements/banner.jsx
@@ -5,6 +5,7 @@ export default function Banner({img, title,className= 'banner'}) {
     <div className={className}>
       <img src={img} alt="Banner" className="Banner" />
       <h2 className='bannerTitle'>{title}</h2>
+      
     </div>
   );
 };
